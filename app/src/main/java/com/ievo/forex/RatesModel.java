2742b0afa11a7f47c372d58d7771f958239c58aa
ievo <ievologin@gmail.com>
2024-06-24 09:53:42 +0700

    Initial commit

---
+++ app/src/main/java/com/ievo/forex/RatesModel.java
@@ -0,0 +1,87 @@
+package com.ievo.forex;
+
+public class RatesModel {
+    private double AUD, BND, BTC, EUR, GBP, HND, IDR, IMR, JPY, MYR;
+
+    public RatesModel(){}
+
+    public double getAUD() {
+        return AUD;
+    }
+
+    public void setAUD(double AUD) {
+        this.AUD = AUD;
+    }
+
+    public double getBND() {
+        return BND;
+    }
+
+    public void setBND(double BND) {
+        this.BND = BND;
+    }
+
+    public double getBTC() {
+        return BTC;
+    }
+
+    public void setBTC(double BTC) {
+        this.BTC = BTC;
+    }
+
+    public double getEUR() {
+        return EUR;
+    }
+
+    public void setEUR(double EUR) {
+        this.EUR = EUR;
+    }
+
+    public double getGBP() {
+        return GBP;
+    }
+
+    public void setGBP(double GBP) {
+        this.GBP = GBP;
+    }
+
+    public double getHND() {
+        return HND;
+    }
+
+    public void setHND(double HND) {
+        this.HND = HND;
+    }
+
+    public double getIDR() {
+        return IDR;
+    }
+
+    public void setIDR(double IDR) {
+        this.IDR = IDR;
+    }
+
+    public double getIMR() {
+        return IMR;
+    }
+
+    public void setIMR(double IMR) {
+        this.IMR = IMR;
+    }
+
+    public double getJPY() {
+        return JPY;
+    }
+
+    public void setJPY(double JPY) {
+        this.JPY = JPY;
+    }
+
+    public double getMYR() {
+        return MYR;
+    }
+
+    public void setMYR(double MYR) {
+        this.MYR = MYR;
+    }
+}
